0da86667459221b70e39c9c92d27578e97e53a2f
Matthieu Cutin <matthieu.cutin@gmail.com>
2015-11-29 18:10:45 +0100

    Update Bundles config

---
+++ src/Bloom/Bundle/CoreBundle/BloomCoreBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bloom\Bundle\CoreBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BloomCoreBundle extends Bundle
+{
+}
---
+++ src/Bloom/Bundle/FrontBundle/BloomFrontBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Bloom\Bundle\FrontBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BloomFrontBundle extends Bundle
+{
+}
